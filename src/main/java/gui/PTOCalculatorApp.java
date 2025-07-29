77b54b5db65ea0485ccb6b3b030d32b7630b09c2
Matthew Vine <mattvine03@gmail.com>
2025-07-28 21:44:45 -0400

    feat: initial tooltip display of pto balance

--- src/main/java/gui/PTOCalculatorApp.java
+++ src/main/java/gui/PTOCalculatorApp.java
@@ -40,6 +40,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.ContextMenu;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseButton;
 import javafx.scene.input.MouseEvent;
@@ -298,8 +299,10 @@ public class PTOCalculatorApp extends Application {
             ZonedDateTime date = monthView.getZonedDateTimeAt(evt.getX(), evt.getY(), calendarView.getZoneId());
             if (date != null && !date.toLocalDate().isBefore(LocalDate.now())) {
                 double balance = ptoCalculator.computeBalanceAtDate(date.toLocalDate(), getFutureEntries());
-                // TODO: Display balance in GUI
-                System.out.println("Balance at start of " + date.toLocalDate() + ": " + balance);
+
+                Tooltip tooltip = new Tooltip(String.format("Projected PTO Balance: %.2f", balance));
+                tooltip.setAutoHide(true);
+                tooltip.show(calendarView.getScene().getWindow(), evt.getScreenX(), evt.getScreenY());
             }
         }
     }
